b5b58e10fcb43960b3465e0e4e478ce9e572c9d0
mohammadnasr <nasrmohammad4804@gmail.com>
2024-10-23 19:03:38 +0330

    initialize crawler-subscriber service

---
+++ crawler-subscriber/src/main/java/com/nasr/crawlersubscriber/CrawlerSubscriberApplication.java
@@ -0,0 +1,13 @@
+package com.nasr.crawlersubscriber;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrawlerSubscriberApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrawlerSubscriberApplication.class, args);
+    }
+
+}
