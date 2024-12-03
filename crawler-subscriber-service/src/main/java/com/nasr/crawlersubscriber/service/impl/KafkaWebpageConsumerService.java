5432f559710f18a6d1894e38c8b1ab031a0421d3
mohammadnasr <nasrmohammad4804@gmail.com>
2024-12-04 02:41:48 +0330

    change service instead only save at mongo also publish message for process at search-service

--- crawler-subscriber-service/src/main/java/com/nasr/crawlersubscriber/service/impl/KafkaWebpageConsumerService.java
+++ crawler-subscriber-service/src/main/java/com/nasr/crawlersubscriber/service/impl/KafkaWebpageConsumerService.java
@@ -1,23 +1,28 @@
 package com.nasr.crawlersubscriber.service.impl;
 
+import com.nasr.crawlersubscriber.dto.ETLData;
 import com.nasr.crawlersubscriber.dto.ExtractedData;
 import com.nasr.crawlersubscriber.entities.ExtractedDataEntity;
 import com.nasr.crawlersubscriber.service.ExtractedDataService;
 import com.nasr.crawlersubscriber.service.KafkaConsumerService;
+import com.nasr.crawlersubscriber.service.KafkaProducerService;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
-import static com.nasr.crawlersubscriber.constant.ApplicationConstant.*;
-import static com.nasr.crawlersubscriber.util.MapperUtility.*;
+import static com.nasr.crawlersubscriber.constant.ApplicationConstant.WEB_PAGE_GROUP_ID;
+import static com.nasr.crawlersubscriber.constant.ApplicationConstant.WEB_PAGE_TOPIC_NAME;
+import static com.nasr.crawlersubscriber.util.MapperUtility.map;
 
 @Component
 @Slf4j
+@RequiredArgsConstructor
 public class KafkaWebpageConsumerService implements KafkaConsumerService {
 
-    @Autowired
-    private ExtractedDataService extractedDataService;
+    private final ExtractedDataService extractedDataService;
+
+    private final KafkaProducerService<ETLData> kafkaProducerService;
 
     @Override
     @KafkaListener(topics = WEB_PAGE_TOPIC_NAME,groupId = WEB_PAGE_GROUP_ID,containerFactory = "containerFactory")
@@ -27,6 +32,19 @@ public class KafkaWebpageConsumerService implements KafkaConsumerService {
                 .apply(data);
 
         ExtractedDataEntity entity = extractedDataService.save(extractedData);
+
+        ETLData etlData = convertEntityToEtlData(entity);
+        kafkaProducerService.publish(etlData);
+
         log.info("extracted data saved with id : {}",entity.getId());
+    }
+
+    private ETLData convertEntityToEtlData(ExtractedDataEntity entity) {
+        return ETLData.builder()
+                .id(entity.getId())
+                .url(entity.getUrl())
+                .title(entity.getTitle())
+                .body(entity.getContent())
+                .build();
     }
 }
