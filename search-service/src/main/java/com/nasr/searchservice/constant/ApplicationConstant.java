684c31bf07dd7a859297c23a6eb8ce8f6ecc0f2c
mohammadnasr <nasrmohammad4804@gmail.com>
2024-12-04 02:33:18 +0330

    add constant for config during project lifecycle

---
+++ search-service/src/main/java/com/nasr/searchservice/constant/ApplicationConstant.java
@@ -0,0 +1,7 @@
+package com.nasr.searchservice.constant;
+
+public class ApplicationConstant {
+
+    public static final String ETL_DATA_TOPIC_NAME = "etl_data";
+    public  static final String ETL_DATA_GROUP_ID="etl_data_group";
+}
---
+++ search-service/src/main/java/com/nasr/searchservice/dto/ETLData.java
@@ -0,0 +1,23 @@
+package com.nasr.searchservice.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.*;
+
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+@Getter
+public class ETLData {
+
+    @JsonProperty(value = "id")
+    private String id;
+    @JsonProperty(value = "title")
+    private String title;
+    @JsonProperty(value = "body")
+    private String body;
+    @JsonProperty(value = "url")
+    private String url;
+
+
+}
