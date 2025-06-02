ecacc70e79ff0b3ab65933295333b79ca2f94a61
mohammadnasr <nasrmohammad4804@gmail.com>
2025-06-02 22:19:47 +0330

    refactor routes.py

--- sentence-transformer-model/app/routes.py
+++ sentence-transformer-model/app/routes.py
@@ -3,9 +3,19 @@ from app.services.EmbedderService import embed_data
 
 api_blueprint = Blueprint('api', __name__)
 
-@api_blueprint.route('/embed', methods=['POST'])
-def embed():
+@api_blueprint.route('/batch-embed', methods=['POST'])
+def batch_embed():
+
     data = request.json
-    text = data['query']
-    vector = embed_data(text)
-    return jsonify(vector)
+    texts = data['texts']
+    embedded_texts = embed_data(texts)
+    return jsonify(embedded_texts)
+
+
+@api_blueprint.route('/embed', methods=['GET'])
+def embed():
+
+    text = request.args.get('data')
+    embedding_text = embed_data(text)
+    return jsonify(embedding_text)
+
