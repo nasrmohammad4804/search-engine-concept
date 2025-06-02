1e04598603772b2337eeab149c7692010da4bb37
mohammadnasr <nasrmohammad4804@gmail.com>
2025-06-02 22:18:31 +0330

    refactor KafkaETLDataConsumerService

--- search-service/src/main/java/com/nasr/searchservice/service/impl/KafkaETLDataConsumerService.java
+++ search-service/src/main/java/com/nasr/searchservice/service/impl/KafkaETLDataConsumerService.java
@@ -1,9 +1,7 @@
 package com.nasr.searchservice.service.impl;
 
 import com.nasr.searchservice.dto.ETLData;
-import com.nasr.searchservice.entities.WebpageEntity;
 import com.nasr.searchservice.service.KafkaConsumerService;
-import com.nasr.searchservice.service.WebpageService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
@@ -15,16 +13,17 @@ import static com.nasr.searchservice.util.MapperUtility.map;
 @Service
 public class KafkaETLDataConsumerService implements KafkaConsumerService {
 
+
     @Autowired
-    private WebpageService webpageService;
+    private EmbeddingBufferServiceImpl embeddingBufferService;
 
     @Override
-    @KafkaListener(topics = ETL_DATA_TOPIC_NAME,groupId =ETL_DATA_GROUP_ID )
+    @KafkaListener(topics = ETL_DATA_TOPIC_NAME, groupId = ETL_DATA_GROUP_ID)
     public void consume(String data) {
 
         ETLData etlData = map(ETLData.class)
                 .apply(data);
 
-        webpageService.save(etlData);
+        embeddingBufferService.addMessage(etlData);
     }
 }
